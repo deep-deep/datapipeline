704eb852ca157004a4d4fc32e66c5fc89a1ba520
Mateo Velez <mateo.velezcobian@gmail.com>
2023-12-26 11:15:07 -0600

    Update flows

--- flows/hello-world.py
+++ flows/hello-world.py
@@ -1,8 +1,25 @@
-from prefect import flow,task
+from prefect import flow,task,deploy
+import pandas as pd
 
 @flow
 def hello_world(s):
     print(f"Hello, {s}!")
+    print(pd.read_csv.__doc__)
     return len(s)
 
 
+if __name__ == "__main__":
+    deploy(
+    hello_world.to_deployment(
+        name = "hello-world-one",
+        cron = "* * * * *",
+        parameters = {"s" : "one"}
+    ),
+    hello_world.to_deployment(
+        name = "hello-world-two",
+        cron = "* * * * *",
+        parameters = {"s":"two"}
+    ),
+    work_pool_name = "test",
+    image = "skppy/hello-world:test"
+    )
